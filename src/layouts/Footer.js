89a66b8fe69d0e2534d13594f67f726f3910b4e1
Lien <tle3212000@163.com>
2024-03-21 23:25:04 +0800

    feat: 更新页脚版本号0.7.4

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -8,7 +8,7 @@ const FooterView = () => (
     <GlobalFooter
       copyright={
         <Fragment>
-          MyData v0.7.3 Copyright <Icon type="copyright" /> 2024 LIEN{' '}
+          MyData v0.7.4 Copyright <Icon type="copyright" /> 2024 LIEN{' '}
           <a
             key="github"
             title="git"
